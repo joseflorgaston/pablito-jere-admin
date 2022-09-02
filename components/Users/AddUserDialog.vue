373ea8a65d840f49e4e0bfda566e3495e39ef35b
Jose Flor Gaston <joseflorgaston@gmail.com>
2022-09-02 02:07:18 -0400

    feat: user page

---
+++ components/Users/AddUserDialog.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-form v-model="isValid">
+        <v-card>
+            <v-toolbar color="primary" dark>
+                <div class="row align-center px-lg-3">
+                    Agregar nuevo usuario
+                    <v-spacer></v-spacer>
+                    <v-btn class="mx-2" fab dark small color="primary" @click="closeDialog">
+                        <v-icon dark>
+                            mdi-close
+                        </v-icon>
+                    </v-btn>
+                </div>
+            </v-toolbar>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12">
+                        <h3 class="mb-2">Nombre de usuario</h3>
+                        <v-text-field v-model="user.userName" :rules="rules" solo outlined placeholder="Nombre de usuario"  maxlength="16">
+                            
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <h3 class="mb-2">Correo</h3>
+                        <v-text-field v-model="user.email" :rules="rules" solo outlined placeholder="Correo"  maxlength="40">
+                            
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                        <h3 class="mb-2">Contrasena</h3>
+                        <v-text-field v-model="user.password" :rules="rules" solo outlined placeholder="Contrasena"  maxlength="12">
+
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+                <v-btn :disabled="!isValid" color="primary" @click="saveUser()">
+                    Guardar
+                </v-btn>
+                <v-btn text @click="closeDialog()">Close</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-form>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { User } from '../../models/User';
+export default Vue.extend ({
+    data() {
+        return {
+            isValid: false,
+            rules: [(v: any) => !!v || 'Este campo es requerido'],
+            user: {
+                userName: "",
+                email: "",
+                password: ""
+            } as User,
+        }
+    },
+    methods: {
+        saveUser() {
+
+        },
+        closeDialog() {
+            this.$emit('closeDialog');
+        },
+    }
+});
+</script>
--- pages/users.vue
+++ pages/users.vue
@@ -1,11 +1,119 @@
 <template>
     <div>
-        <h2>Usuarios</h2>
+        <div class="d-flex justify-space-between pa-1 pb-3">
+            <h2>Usuarios</h2>
+            <v-btn color="secondary" @click="showAddUserModal()">
+                <v-icon>mdi-plus</v-icon>
+                Agregar
+            </v-btn>
+        </div>
+        <v-card>
+            <v-data-table 
+                :headers="tableHeaders" :items="items" :page="page" :items-per-page="itemsPerPage"
+                :loading="loading" :footer-props="{
+                    itemsPerPageOptions: [5, 10, 15],
+                }">
+                <template #[`item.actions`]=item>
+                    <v-tooltip bottom>
+                        <template #activator="{ on, attrs }">
+                            <v-btn icon v-bind="attrs" v-on="on" @click="openEditDialog(item)">
+                                <v-icon color="primary" title="editar">mdi-pencil</v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Editar Usuario</span>
+                    </v-tooltip>
+                    <v-tooltip bottom>
+                        <template #activator="{ on, attrs }">
+                            <v-btn 
+                                icon :disabled="item.inOrder > 0" v-bind="attrs" @click="showOrCloseDeleteDialog(item)"
+                                v-on="on">
+                                <v-icon color="error" title="eliminar">mdi-delete</v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Eliminar Usuario</span>
+                    </v-tooltip>
+                </template>
+            </v-data-table>
+        </v-card>
+        <v-dialog v-model="showAddDialog" transition="dialog-top-transition" max-width="100%" width="600" persistent>
+            <AddUserDialog @getPublications="getUsers()" @closeDialog="showAddUserModal()">
+            </AddUserDialog>
+        </v-dialog>
+        <v-dialog v-model="showDeleteDialog" transition="dialog-top-transition" max-width="100%" width="500" persistent>
+            <DeleteDialog v-bind="deleteOptions" @getPublications="getUsers()" @closeDialog="showOrCloseDeleteDialog({})">
+            </DeleteDialog>
+        </v-dialog>
     </div>
 </template>
-
 <script lang="ts">
-export default ({
+import Vue from 'vue';
+import DeleteDialog from '~/components/Shared/DeleteDialog.vue';
+import AddUserDialog from '~/components/Users/AddUserDialog.vue';
+
+export default Vue.extend({
     name: 'UsersComponent',
+    components: { DeleteDialog, AddUserDialog },
+    middleware: 'auth',
+    data() {
+        return {
+            count: 0,
+            page: 0,
+            itemsPerPage: 10,
+            loading: true,
+            showDeleteDialog: false,
+            showAddDialog: false,
+            deleteOptions: {},
+            items: [],
+            tableHeaders: [
+                {
+                    text: 'Nombre de usuario',
+                    value: 'userName',
+                    class: 'header-color white--text',
+                },
+                {
+                    text: 'Email',
+                    value: 'email',
+                    class: 'header-color white--text',
+                },
+                {
+                    text: 'Acciones',
+                    value: 'actions',
+                    class: 'header-color white--text',
+                },
+            ],
+        }
+    },
+    async beforeMount() {
+        try {
+            this.$store.commit('setLoading');
+            const response = await this.$axios.get("accounts/users");
+            this.items = response.data.users;
+            this.count = response.data.count;
+        } catch (error) {
+
+        } finally {
+            this.loading = false;
+            this.$store.commit('setLoading');
+        }
+    },
+    methods: {
+        async getUsers() {
+
+        },
+        showAddUserModal() {
+            this.showAddDialog = !this.showAddDialog;
+        },
+        showOrCloseDeleteDialog(user: any) {
+            this.deleteOptions = {
+                title: "Usuario",
+                name: user.userName,
+                removeUrl: `publications/${user.id}`
+            }
+            this.showDeleteDialog = !this.showDeleteDialog;
+        },
+        openEditDialog(item: any) {
+            console.log(item);
+        }
+    }
 })
 </script>
